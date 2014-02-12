d66f1d53075d0ec3ed04c1424be717cf8e31d80a
Martin Aarhof <martin.aarhof@gmail.com>
2014-02-12 15:04:32 +0100

    changed some stuff around

--- src/Jwapi_Tests/bootstrap.php
+++ src/Jwapi_Tests/bootstrap.php
@@ -17,15 +17,20 @@ if (! $filefound) {
     throw new \Exception('Could not load dependencies');
 }
 
-$configfile = __DIR__ . '/../../config.yml';
-if (!file_exists($configfile)) {
-    throw new \Exception('Could not load config file - create a config.yml file');
+if (getenv('TRAVIS') && getenv('TRAVIS_KEY') && getenv('TRAVIS_SECRET')) {
+    define('JWTEST_APIKEY', getenv('TRAVIS_KEY'));
+    define('JWTEST_APISECRET', getenv('TRAVIS_SECRET'));
 } else {
-    $yaml = \Symfony\Component\Yaml\Yaml::parse(file_get_contents($configfile));
-    if (array_key_exists('apikey', $yaml) && array_key_exists('apisecret', $yaml)) {
-        define('JWTEST_APIKEY', $yaml['apikey']);
-        define('JWTEST_APISECRET', $yaml['apisecret']);
+    $configfile = __DIR__ . '/../../config.yml';
+    if (!file_exists($configfile)) {
+        throw new \Exception('Could not load config file - create a config.yml file');
     } else {
-        throw new \Exception('Could not load config file - apikey and apisecret needs to be in the config file');
+        $yaml = \Symfony\Component\Yaml\Yaml::parse(file_get_contents($configfile));
+        if (array_key_exists('apikey', $yaml) && array_key_exists('apisecret', $yaml)) {
+            define('JWTEST_APIKEY', $yaml['apikey']);
+            define('JWTEST_APISECRET', $yaml['apisecret']);
+        } else {
+            throw new \Exception('Could not load config file - apikey and apisecret needs to be in the config file');
+        }
     }
 }
